2bac184d03a91bc44460b03c7a1241f8d037ff0b
Robert <rob.justin.wagner@gmail.com>
2015-02-23 15:58:25 -0600

    timer for splash screen; STILL NEED TO MOVE SOCKET IO OVER TO LOGIN ACTIVITY

--- app/src/main/java/com/mamba/grapple/MainActivity.java
+++ app/src/main/java/com/mamba/grapple/MainActivity.java
@@ -12,6 +12,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
 // *socket.io imports*
 import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.Socket;
@@ -37,13 +42,28 @@ import javax.net.ssl.SSLPeerUnverifiedException;
 
 public class MainActivity extends ActionBarActivity {
 
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
     public final Gson gson = new Gson();
-    public final static String EXTRA_MESSAGE = "com.mamba.tutorial.MESSAGE";
+    public final static String EXTRA_MESSAGE = "com.mamba.grapple.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(MainActivity.this,LoginActivity.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 
 
