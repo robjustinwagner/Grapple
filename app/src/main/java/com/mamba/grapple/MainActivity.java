ac32a3d5ef12c8a121909495a5959326fbddb276
Robert <rob.justin.wagner@gmail.com>
2015-03-06 15:11:20 -0600

    STILL NEED TO MOVE SOCKET IO OVER TO LOGIN ACTIVITY

--- app/src/main/java/com/mamba/grapple/MainActivity.java
+++ app/src/main/java/com/mamba/grapple/MainActivity.java
@@ -40,7 +40,7 @@ import java.security.cert.Certificate;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLPeerUnverifiedException;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity {
 
     /**
      * Duration of wait *
@@ -50,10 +50,12 @@ public class MainActivity extends ActionBarActivity {
     public final Gson gson = new Gson();
     public final static String EXTRA_MESSAGE = "com.mamba.grapple.MESSAGE";
 
-    @Override
+    //@Override
     protected void onCreate(Bundle savedInstanceState) {
+        /*
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        */
 
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/
@@ -61,13 +63,16 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
+                /*
                 Intent mainIntent = new Intent(MainActivity.this, LoginActivity.class);
                 MainActivity.this.startActivity(mainIntent);
                 MainActivity.this.finish();
+                */
             }
         }, SPLASH_DISPLAY_LENGTH);
     }
 }
+
 /* REMOVE THE ABOVE } TO FIX!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
